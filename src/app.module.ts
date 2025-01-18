e284739a83ed515b5157ea7ddfab9b2bb4fbc692
Vitalii Kostenko <apelsin@i.ua>
2025-01-18 18:09:23 +0100

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { FormBuilderModule } from './form-builder/form-builder.module';
 import { ConfigModule } from '@nestjs/config';
 import dynamodbConfig from '../config/dynamodb.config';
 import { DynamodbService } from './common-services/dynamodb/dynamodb.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { DynamodbService } from './common-services/dynamodb/dynamodb.service';
     ConfigModule.forRoot({
       load: [dynamodbConfig],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, DynamodbService],
