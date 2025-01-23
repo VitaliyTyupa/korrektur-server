dbf2f06b4acea019d35fc950da3d2f42dc6aea27
Vit Tiupa <vitaliy.t@zazmic.com>
2025-01-23 16:53:42 +0100

    Added base request to openAi

--- src/chatgpt/chatgpt/chatgpt.controller.ts
+++ src/chatgpt/chatgpt/chatgpt.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ChatgptService } from './chatgpt.service';
 
-@Controller('chatgpt')
+@Controller('ai-factory')
 export class ChatgptController {
   constructor(private readonly chatgptService: ChatgptService) {}
 
@@ -9,6 +9,16 @@ export class ChatgptController {
   async checkSentence(@Body() data: { message: string }) {
     try {
       return await this.chatgptService.checkSentence(data.message);
+    } catch (error) {
+      console.error('Error processing request:', error);
+      throw new Error('Internal server error');
+    }
+  }
+
+  @Post('generate-text')
+  async generateText(@Body() data: any) {
+    try {
+      return await this.chatgptService.generateText(data);
     } catch (error) {
       console.error('Error processing request:', error);
       throw new Error('Internal server error');
--- src/chatgpt/chatgpt/chatgpt.service.ts
+++ src/chatgpt/chatgpt/chatgpt.service.ts
@@ -57,4 +57,88 @@ export class ChatgptService {
     const responce = completion.choices[0];
     return { message: `Checked sentence: ${responce.message.content}` };
   }
+
+  async generateText(message: any): Promise<any> {
+    const openAIKey = process.env.OPENAI_API_KEY;
+    const openai = new OpenAI({
+      apiKey: openAIKey,
+    });
+    const targetLanguage = 'German';
+    const laguageLevel = 'A2';
+    const text = message.text;
+    const source_words = 'herausfinden, anwenden, verwenden';
+    const countOfSentences = 5;
+    const sourceLanguage = 'Ukrainian';
+
+    console.log(message);
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-4o',
+      messages: [
+        {
+          role: 'system',
+          content: [
+            {
+              text: `You are an expert ${targetLanguage} language teacher tasked with
+              generating content for exercises. You will create grammatically
+              and stylistically correct sentences that align with the context
+              of the original input text. Lexical and grammatical constructions
+              should adhere to the ${laguageLevel} language level.
+
+              Input Details:
+              - input_text (context in ${targetLanguage}): "{{Original ${targetLanguage} text provided by the user}}"
+              - source_words (in ${targetLanguage}) to be used: "{{list of selected words}}"
+              - name_of_task: "{{description of task}}"
+
+              Return the response in the following JSON format:
+              {
+                "language_level": "{{language level}}",
+                "context_of_original_text": "{{summary or main topic of the original text}}",
+                "used_source_words": "{{selected words from the user that were used}}",
+                "tasks": "{{task result generated based on the input}}"
+              }`,
+              type: 'text',
+            },
+          ],
+        },
+        {
+          role: 'user',
+          content: [
+            {
+              type: 'text',
+              text: `input_text : ${text}
+                source_words: ${source_words}
+                task_1: Generate ${countOfSentences} new sentences using source_words. Place the
+                source_words in square brackets within the generated sentences.
+                task_2: Provide definitions of the source_words in ${targetLanguage}.
+                Provide response in json format {word: one of source_words,
+                definition: definition of word}
+                task_3:  Create ${countOfSentences} True/False statements. Provide response in
+                json format - correct ansver in square brackets as a key: statement as a value
+                task_4:  Generate ${countOfSentences} questions with antworts to input_text.
+                Provide response in json format as an array of entity
+                where {question: generated question, antwort: antwort for
+                this question}
+                task_5:  Generate ${countOfSentences} sentences in ${sourceLanguage}.
+                Provide response in json format as an array of entity where
+                {source: generated sentence in ${sourceLanguage}, translation: correct translation in ${targetLanguage}}
+`,
+            },
+          ],
+        },
+      ],
+      response_format: {
+        type: 'json_object',
+      },
+      temperature: 1,
+      max_completion_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    console.log(completion.choices[0]);
+    const responce = completion.choices[0];
+    return JSON.stringify(responce.message.content);
+    // return { message: 'Generated text' };
+  }
 }
