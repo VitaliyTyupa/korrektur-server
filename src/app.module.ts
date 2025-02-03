ca28b2430372361ea5debe5835f321ffc7a09b4c
Vit Tiupa <vitaliy.t@zazmic.com>
2025-02-03 20:20:37 +0100

    Updated Prompts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FormBuilderModule } from './form-builder/form-builder.module';
 import { ConfigModule } from '@nestjs/config';
-import dynamodbConfig from '../config/dynamodb.config';
 import { DynamodbService } from './common-services/dynamodb/dynamodb.service';
 import { AiFactoryModule } from './ai-factory/ai-factory.module';
 import { AuthModule } from './auth/auth.module';
+import dynamodbConfig from './config/dynamodb.config';
 
 @Module({
   imports: [
