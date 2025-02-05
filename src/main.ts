657b39b62fc65d0c5f0214a361db141da8b9d243
Vit Tiupa <vitaliy.t@zazmic.com>
2025-02-05 10:29:53 +0100

    Added CI/CD and enableCors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://dl-client-bbac8ef1f1.s3-website.eu-north-1.amazonaws.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3000);
   console.log('server listening port:3000');
 }
