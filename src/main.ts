21e6e5c42ecc02d5df6572a6d3ad8508d575c022
Vit Tiupa <vitaliy.t@zazmic.com>
2025-02-05 17:07:11 +0100

    Updated deploy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 // set .env variables to process.env
 dotenv.config();
+console.log(process.env.AWS_REGION, process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
